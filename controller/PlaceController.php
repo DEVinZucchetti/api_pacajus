307b5e9fdb4ee74318cbb65b183cb1c1d9b1c9c5
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-10-27 20:13:38 -0300

    adiciona codigo do exercicio

--- controller/PlaceController.php
+++ controller/PlaceController.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'utils.php';
-require_once 'models/Place.php';
+require_once '../utils.php';
+require_once '../models/Place.php';
+require_once '../models/PlaceDAO.php';
 
 class PlaceController
 {
@@ -20,6 +21,7 @@ class PlaceController
             responseError('Faltaram informações essenciais', 400);
         }
 
+        /*
         $allData = readFileContent(FILE_CITY);
 
         $itemWithSameName = array_filter($allData, function ($item) use ($name) {
@@ -29,13 +31,9 @@ class PlaceController
         if (count($itemWithSameName) > 0) {
             responseError('O item já existe', 409);
         }
+        */
 
-        //  foreach ($places as $place) {
-        //     if ($place->name === $name) {
-        //         echo json_encode(['error' => 'Este lugar já está cadastrado.']);
-        //         exit;
-        //     }
-        // }
+        // select * from places where name = 'douglas'
 
         $place = new Place($name);
         $place->setContact($contact);
@@ -43,16 +41,23 @@ class PlaceController
         $place->setDescription($description);
         $place->setLatitude($latitude);
         $place->setLongitude($longitude);
-        $place->save();
 
-        response(['message' => 'cadastrado com sucesso'], 201);
+        $placeDAO = new PlaceDAO();
+        $result = $placeDAO->insert($place);
+
+        if ($result['success'] === true) {
+            response(["message" => "Cadastrado com sucesso"], 201);
+        } else {
+            responseError("Não foi possível realizar o cadastro", 400);
+        }
     }
 
 
     public function list()
     {
-        $places = (new Place())->list();
-        response($places, 200);
+        $placeDAO = new PlaceDAO();
+        $result = $placeDAO->findMany();
+        response($result, 200);
     }
 
     public function delete()
@@ -63,8 +68,8 @@ class PlaceController
             responseError('ID ausente', 400);
         }
 
-        $place = new Place();
-        $place->delete($id);
+        $placeDAO = new PlaceDAO();
+        $placeDAO->deleteOne($id);
 
         response(['message' => 'Deletado com sucesso'], 204);
     }
@@ -77,23 +82,24 @@ class PlaceController
             responseError('ID ausente', 400);
         }
 
-        $place = new Place();
-        $item = $place->listOne($id);
+        $PlaceDAO = new PlaceDAO();
+        $item = $PlaceDAO->findOne($id);
 
         response($item, 200);
     }
 
-    public function update(){
+    public function update()
+    {
         $body = getBody();
         $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
-    
+
         if (!$id) {
             responseError('ID ausente', 400);
         }
-    
-        $place = new Place();
-        $place->update($id, $body);
-    
+
+        $placeDAO = new PlaceDAO();
+        $placeDAO->updateOne($id, $body);
+
         response(['message' => 'atualizado com sucesso'], 200);
     }
 }
